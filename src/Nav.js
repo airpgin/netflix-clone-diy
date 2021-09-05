9bdfaefbfd7193362af5472d03cecfa4f32ca044
Asher <airpgin@gmail.com>
2021-09-05 18:15:40 +0800

    home page

---
+++ src/Nav.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import './Nav.css'
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavBar)
+    return () => {
+      window.removeEventListener('scroll', transitionNavBar)
+    }
+  }, [])
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav__content">
+        <img className="nav__logo" src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo-700x394.png" alt="" />
+        <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCEnEQbp7vKS2krza7YbQqsTrKjfbYywSE",
+  authDomain: "netflix-clone-diy.firebaseapp.com",
+  projectId: "netflix-clone-diy",
+  storageBucket: "netflix-clone-diy.appspot.com",
+  messagingSenderId: "837345528089",
+  appId: "1:837345528089:web:fc848c86780b0afbbddd88"
+};
